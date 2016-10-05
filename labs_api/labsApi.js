86fd6f426d8fc07c7118fc3aa75dafb0c6e738b6
johneas10 <easton3003@gmail.com>
2016-10-05 14:56:02 +0100

    get route added

--- labs_api/labsApi.js
+++ labs_api/labsApi.js
@@ -1,14 +1,19 @@
 var LabsApi = function(app) {
 
-  var labsApiData = [ "mince", "steak", "macaroni", "chicken & leek" ];
+  var pies = [ "mince", "steak", "macaroni", "chicken & leek" ];
 
-  app.get('/pies', function(req, res) {
-    res.json({ text: " here are the Pies!" });
+  // app.get('/pies', function(req, res) {
+  //   res.json({ text: " here are the Pies!" });
+  // });
+
+  app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
   });
 
-  // app.get("/", function(req,res){
-  //   res.sendFile(path.join(__dirname + "/client/build/index.html"));
-  // });
+  app.get("/api/pies", function(req, res){
+    // query.all(function(results))
+    res.json({data: pies});
+  })
 
 }
 
